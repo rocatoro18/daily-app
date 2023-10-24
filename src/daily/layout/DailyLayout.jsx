50175f9cc7eff886770b8c2f492a8f669b384ca4
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-10-23 22:18:37 -0700

    Navbar

--- src/daily/layout/DailyLayout.jsx
+++ src/daily/layout/DailyLayout.jsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/material';
+import { NavBar } from '../components/NavBar';
 
 const drawerWidth = 240;
 
@@ -7,6 +8,7 @@ export const DailyLayout = ({children}) => {
         <Box sx={{display: 'flex'}}>
 
         {/* Navbar drawerWidth */}
+        <NavBar drawerWidth={drawerWidth}/>
 
         {/* Sidebar drawerWidth */}
 
