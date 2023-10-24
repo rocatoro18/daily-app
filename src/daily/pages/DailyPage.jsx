defd7cc75c45d8504bedeb4145075bb07ce613b5
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-10-24 00:52:44 -0700

    NoteView

--- src/daily/pages/DailyPage.jsx
+++ src/daily/pages/DailyPage.jsx
@@ -1,6 +1,6 @@
 import { Typography } from '@mui/material';
 import { DailyLayout } from '../layout/DailyLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 
 export const DailyPage = () => {
@@ -8,8 +8,9 @@ export const DailyPage = () => {
         <DailyLayout>
             {/*<Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur sed blanditiis quo. Libero ratione totam ullam culpa ea quasi quod dolorem molestiae. Fugiat, id beatae veniam possimus laudantium nesciunt praesentium?</Typography>*/}
             {/* NothingSelected */}
-            <NothingSelectedView/>
+            {/*<NothingSelectedView/>*/}
             {/* NoteView */}
+            <NoteView/>
         </DailyLayout>
     )
 }
