b80bfc2bdabdf7fe0b5660ca806535ace62689c3
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-10-24 01:19:50 -0700

    Boton flotante

--- src/daily/pages/DailyPage.jsx
+++ src/daily/pages/DailyPage.jsx
@@ -1,6 +1,7 @@
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import { DailyLayout } from '../layout/DailyLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 
 export const DailyPage = () => {
@@ -8,9 +9,24 @@ export const DailyPage = () => {
         <DailyLayout>
             {/*<Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur sed blanditiis quo. Libero ratione totam ullam culpa ea quasi quod dolorem molestiae. Fugiat, id beatae veniam possimus laudantium nesciunt praesentium?</Typography>*/}
             {/* NothingSelected */}
-            {/*<NothingSelectedView/>*/}
+            <NothingSelectedView/>
             {/* NoteView */}
-            <NoteView/>
+            {/*<NoteView/>*/}
+
+            <IconButton
+                size='large'
+                sx={{
+                    color:'white',
+                    backgroundColor:'error.main',
+                    ':hover':{backgroundColor:'error.main',opacity:0.9},
+                    position:'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{fontSize:30}}/>
+            </IconButton>
+
         </DailyLayout>
     )
 }
