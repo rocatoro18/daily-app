ec8f19a7558d4f8d032783c0501f695c00011883
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-10-28 21:57:52 -0700

    Custom hook validar formularios

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -24,7 +24,7 @@ export const RegisterPage = () => {
         formState, displayName, email, password, onInputChange,
         isFormValid, displayNameValid, emailValid, passwordValid
     } = useForm(formData, formValidations);
-
+    console.log(displayNameValid);
     const onSubmit = (event) => {
         event.preventDefault();
         console.log(formState);
